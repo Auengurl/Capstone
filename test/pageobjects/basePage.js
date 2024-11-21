c0f505ab1726f4ac08122d1bcdd12f1987113749
audreyChecketts <achkts@gmail.com>
2024-11-21 10:30:54 -0700

    headerSelector added

--- test/pageobjects/basePage.js
+++ test/pageobjects/basePage.js
@@ -1,17 +1,24 @@
 import { browser } from '@wdio/globals';
-
+import { $ } from '@wdio/globals';
+ 
 export default class BasePage {
     
     openBasePage () {
         return browser.url(`https://www.dragonsteelbooks.com/`);
     }
-
+    get selectorDetector () {
+        return $('.text-animation--underline-in-header');
+    }
 
     async openPage(headerLink, expectedUrl) {
         await this.openBasePage(); 
         await headerLink.click();  
         await expect(browser).toHaveUrl(expectedUrl); 
         await browser.pause(3000); 
+    }
+
+    async headerSelector () {
+        await  expect(this.selectorDetector());
     }
 }
 
