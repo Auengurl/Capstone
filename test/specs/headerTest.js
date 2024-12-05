5e9d0ec8e54473824774d04d1896b3821a9ce760
audreyChecketts <achkts@gmail.com>
2024-12-05 10:36:22 -0700

    testing sortby

--- test/specs/headerTest.js
+++ test/specs/headerTest.js
@@ -27,33 +27,33 @@ import SortBy from "../pageobjects/filters.js";
 //     });
 // });
 
-describe ('open products pages from drop menu', () => {
-    it('open catalog menu', async () => {
-        await NavHeader.subMenuOpen();
-    });
-
-    it('click each submenu item and verify the Url', async () => {
-        const menuItems = NavHeader.menuItems; 
-
-        for (const item of menuItems) {
-            const { title, expectedUrl } = item; 
-            await NavHeader.selectProductSubMenuOpenExpectUrl(await title, expectedUrl);
-            
-        }
-    });
-})
-
-
-
-// describe('sort by function',() => {
-//     it('sort by menu opens on product page', async () => {
-//         await SortBy.sortByFiltersMenu();
+// describe ('open products pages from drop menu', () => {
+//     it('open catalog menu', async () => {
+//         await NavHeader.subMenuOpen();
 //     });
 
-//     // it('sort by selectors reorganize products accordingly', async () => {
-//     //     await SortBy.sortByDisplayed();
-//     // });
-// });
+//     it('click each submenu item and verify the Url', async () => {
+//         const menuItems = NavHeader.menuItems; 
+
+//         for (const item of menuItems) {
+//             const { title, expectedUrl } = item; 
+//             await NavHeader.selectProductSubMenuOpenExpectUrl(await title, expectedUrl);
+            
+//         }
+//     });
+// })
+
+
+
+describe('sort by function',() => {
+    it('sort by menu opens on product page', async () => {
+        await SortBy.sortByFiltersMenu();
+    });
+
+    // it('sort by selectors reorganize products accordingly', async () => {
+    //     await SortBy.sortByDisplayed();
+    // });
+});
 
 
 
