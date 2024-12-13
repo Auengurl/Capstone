b31f8eaa17d4f4f8ee8c1c3b023f537207e2af6a
audreyChecketts <achkts@gmail.com>
2024-12-13 10:03:38 -0700

    changed the only part 8pass 3fail

--- test/specs/cartTest_shipping.js
+++ test/specs/cartTest_shipping.js
@@ -5,7 +5,7 @@ import CartArea from '../pageobjects/cart.js';
 describe('cart page and components', () => {
     
 
-    it.only('shipping link opens from checkout page', async () => {
+    it('shipping link opens from checkout page', async () => {
         await CartArea.shippingPageOpen();
     })
 
